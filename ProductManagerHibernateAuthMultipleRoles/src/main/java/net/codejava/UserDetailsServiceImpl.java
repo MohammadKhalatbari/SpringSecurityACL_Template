bc11d3f114f435e61091c4b2351a60b80c7cd556
Mohammad Khalatbari <khalatbari.computer@gmail.com>
2022-12-14 18:57:28 +0200

    final commit

---
+++ ProductManagerHibernateAuthMultipleRoles/src/main/java/net/codejava/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package net.codejava;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username)
+//	As you can see, this class makes use an instance of UserRepository interface in the loadUserByUsername()
+//	method which will be invoked by Spring Security when authenticating the users.
+			throws UsernameNotFoundException {
+		User user = userRepository.getUserByUsername(username);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException("Could not find user");
+		}
+		
+		return new MyUserDetails(user);
+	}
+
+}
---
+++ ProductManagerHibernateAuthMultipleRoles/src/main/java/net/codejava/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package net.codejava;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsServiceImpl();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+//		we configure HTTP Security for authentication and authorization.
+//		We also configure a custom URL for displaying access denied error in case the users do not have permission.
+		http.authorizeRequests()
+			.antMatchers("/").hasAnyAuthority("ADMIN")
+			.antMatchers("/new").hasAnyAuthority("ADMIN", "CREATOR")
+			.antMatchers("/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
+			.antMatchers("/delete/**").hasAuthority("ADMIN")
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().permitAll()
+			.and()
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/403")
+			;
+	}
+}
